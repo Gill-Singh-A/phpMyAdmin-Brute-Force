26a084501781655151d7829427594e2c260ac12b
Gill-Singh-A <amansg22@iitk.ac.in>
2025-01-11 15:27:03 +0530

    Implemented Command-Line Arguments

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ status_color = {
     ' ': Fore.WHITE
 }
 
+scheme = "http"
+
 def display(status, data, start='', end='\n'):
     print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
 
@@ -23,4 +25,65 @@ def get_arguments(*args):
     return parser.parse_args()[0]
 
 if __name__ == "__main__":
-    pass
+    arguments = get_arguments(('-s', "--server", "server", "Target phpMyAdmin Servers (seperated by ',' or File Name)"),
+                              ('-u', "--users", "users", "Target Users (seperated by ',') or File containing List of Users"),
+                              ('-P', "--password", "password", "Passwords (seperated by ',') or File containing List of Passwords"),
+                              ('-c', "--credentials", "credentials", "Name of File containing Credentials in format ({user}:{password})"),
+                              ('-S', "--scheme", "scheme", f"Scheme to use (Default={scheme})"),
+                              ('-t', "--timeout", "timeout", "Timeout for Login Request"),
+                              ('-w', "--write", "write", "CSV File to Dump Successful Logins (default=current data and time)"))
+    if not arguments.server:
+        display('-', f"Please specify {Back.YELLOW}Target Servers{Back.RESET}")
+        exit(0)
+    else:
+        try:
+            with open(arguments.server, 'r') as file:
+                arguments.server = [server for server in file.read().split('\n') if server != '']
+        except FileNotFoundError:
+            arguments.server = arguments.server.split(',')
+        except Exception as error:
+            display('-', f"Error Occured while reading File {Back.MAGENTA}{arguments.server}{Back.RESET} => {Back.YELLOW}{error}{Back.RESET}")
+            exit(0)
+    if not arguments.credentials:
+        if not arguments.users:
+            display('*', f"No {Back.MAGENTA}USER{Back.RESET} Specified")
+            arguments.users = ['']
+        else:
+            try:
+                with open(arguments.users, 'r') as file:
+                    arguments.users = [user for user in file.read().split('\n') if user != '']
+            except FileNotFoundError:
+                arguments.users = arguments.users.split(',')
+            except:
+                display('-', f"Error while Reading File {Back.YELLOW}{arguments.users}{Back.RESET}")
+                exit(0)
+            display(':', f"Users Loaded = {Back.MAGENTA}{len(arguments.users)}{Back.RESET}")
+        if not arguments.password:
+            arguments.password = ['']
+        elif arguments.password != ['']:
+            try:
+                with open(arguments.password, 'r') as file:
+                    arguments.password = [password for password in file.read().split('\n') if password != '']
+            except FileNotFoundError:
+                arguments.password = arguments.password.split(',')
+            except:
+                display('-', f"Error while Reading File {Back.YELLOW}{arguments.password}{Back.RESET}")
+                exit(0)
+            display(':', f"Passwords Loaded = {Back.MAGENTA}{len(arguments.password)}{Back.RESET}")
+        arguments.credentials = []
+        for user in arguments.users:
+            for password in arguments.password:
+                arguments.credentials.append([user, password])
+    else:
+        try:
+            with open(arguments.credentials, 'r') as file:
+                arguments.credentials = [[credential.split(':')[0], ':'.join(credential.split(':')[1:])] for credential in file.read().split('\n') if len(credential.split(':')) > 1]
+        except:
+            display('-', f"Error while Reading File {Back.YELLOW}{arguments.credentials}{Back.RESET}")
+            exit(0)
+    arguments.scheme = arguments.scheme if arguments.scheme else scheme
+    arguments.timeout = float(arguments.timeout) if arguments.timeout else None
+    if not arguments.write:
+        arguments.write = f"{date.today()} {strftime('%H_%M_%S', localtime())}.csv"
+    display('+', f"Total Servers     = {Back.MAGENTA}{len(arguments.server)}{Back.RESET}")
+    display('+', f"Total Credentials = {Back.MAGENTA}{len(arguments.credentials)}{Back.RESET}")
